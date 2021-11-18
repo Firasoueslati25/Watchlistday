59b72be1a9e983dc76cb5f1fa5afeef4c7e100df
Firas Oueslati <firas.oueslati@audencia.com>
2021-11-18 14:45:29 +0100

    changeeee

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,4 @@
 class List < ApplicationRecord
-  validates :title, :overview, presence: :true
 
   has_many :bookmarks
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,3 @@
 class Movie < ApplicationRecord
-  validates :title, :overview, presence: :true
   has_many :bookmarks
 end
