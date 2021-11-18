405e8321112d3d1ca888fd22e6eacb03e7e3a1fb
Firas Oueslati <firas.oueslati@audencia.com>
2021-11-18 15:00:48 +0100

    ALL GREENN

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   validates_uniqueness_of :movie_id, scope: :list_id
   belongs_to :movie
   belongs_to :list
-  # validates :comment.length, inclusion: { in: 0..5 }
+  validates :comment.length, inclusion: { in: 0..5 }
 end
